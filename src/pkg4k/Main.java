8e27486dfa75e79716044440b42bc4f04a74a695
4kann <frknpnrbs911@gmail.com>
2017-09-22 20:07:05 +0300

    dort islem

---
+++ src/pkg4k/Main.java
@@ -0,0 +1,26 @@
+
+
+
+package pkg4k;
+
+
+        
+    public class Main {
+
+   public static void int topla
+           topla(int=a,int=b)
+              
+   
+    
+    public static void main(String[] args) {
+        int a=1 ;
+        int b=1 ;
+        int c=a*b ;
+        System.out.println("c="+c);
+        
+        
+        
+        
+    }
+    
+}
